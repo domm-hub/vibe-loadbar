16156b00001649c0c01735111eeb445634d1784f
Adam Hany <adam.afify13@gmail.com>
2026-03-05 20:58:22 +0200

    Incremented version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vibe-loadbar",
-    version="0.3.0",
+    version="0.3.5",
     author="Adam Hany",
     description="A lightweight alternative to TQDM. Part of the vibe suite.",
     long_description=open("README.md").read(),
